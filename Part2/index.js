ecd94dcf0f126fa5c875b6f259647a3d7ad2e796
Than <than.jarvis@gmail.com>
2019-09-16 15:47:50 -0600

    all done except the black diamond

---
+++ Part2/index.js
@@ -0,0 +1,9 @@
+const idInput = document.getElementById('idInput')
+const idColor = document.getElementById('colorInput')
+
+function setCard(){
+  const card = document.getElementById(idInput.value)
+  card.style.color = colorInput.value
+    
+
+}
